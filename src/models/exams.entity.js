7fc7f81da604e2a30c33452df3c3941afc783cd6
Ro <u202213765@upc.edu.pe>
2024-10-01 13:01:29 -0500

    fix: added just constructor.

--- src/models/exams.entity.js
+++ src/models/exams.entity.js
@@ -1,44 +1,14 @@
 export class Exams{
-
-    //#region Attributes
-    id: number;
-    patientId: number;
-    examinerId: number;
-    examDate: string;
-    orientationScore: number;
-    registrationScore: number;
-    attentionAndCalculationScore: number;
-    recallScore: number;
-    languageScore: number;
-
-    constructor(id: number, patientId: number, examinerId: number, examDate:string, orientationScore: number, registrationScore: number, attentionAndCalculationScore: number, recallScore: number, languageScore: number) {
+    constructor(id: 0, patientId: 0, examinerId: 0, examDate:'', orientationScore: 0, registrationScore: 0, attentionAndCalculationScore: 0, recallScore: 0, languageScore: 0) {
         this.id = id;
         this.patientId = patientId;
-        this.examinerId = examinerId;
-        this.examDate = examDate;
+        this.examinerId = examDate;
         this.orientationScore = orientationScore;
         this.registrationScore = registrationScore;
         this.attentionAndCalculationScore = attentionAndCalculationScore;
         this.recallScore = recallScore;
         this.languageScore = languageScore;
-    }
-
-    //#region Methods
-
-    getTotalScore():number{
-        return (this.orientationScore + this.registrationScore + this.attentionAndCalculationScore + this.recallScore+ this.languageScore)
 
     }
-
-    getAverageScore(exams: Exams[]): number{
-        const totalScores = exams.reduce((sum, exam)=> sum+exam.getTotalScore(), 0);
-        return exams.length > 0 ? totalScores / exams.length: 0;
-    }
-
-    getExamsById(exams: Exams[], examinerId: number): number{
-        return exams.filter((exam) => exam.examinerId === examinerId).length;
-    }
-
-
-
 }
+
